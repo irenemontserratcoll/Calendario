354072e5f5d286d866854a195236307a9ac30b78
Irene Montserrat <irene.montserrat@opendeusto.es>
2022-11-02 17:14:59 +0100

    Creación de la clase Categoría y Usuario, con sus respectivos get, set y constructores

--- Calendario/src/Categoria.java
+++ Calendario/src/Categoria.java
@@ -1,4 +1,56 @@
+import java.awt.Color;
 
 public class Categoria {
+	
+	//Atributos
+	protected String categoria;
+	protected Usuario autorizados;
+	protected Color color;
+	
+	//Constructores
+	public Categoria(String categoria, Usuario autorizados, Color color) {
+		super();
+		this.categoria = categoria;
+		this.autorizados = autorizados;
+		this.color = color;
+	}
+	
+	public Categoria(String categoria) {
+		this.categoria = categoria;
+		this.autorizados = null;
+		this.color = color.LIGHT_GRAY;
+	}
+		
+	//Get y set
+	public String getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+
+	public Usuario getAutorizados() {
+		return autorizados;
+	}
+
+	public void setAutorizados(Usuario autorizados) {
+		this.autorizados = autorizados;
+	}
+
+	public Color getColor() {
+		return color;
+	}
+
+	public void setColor(Color color) {
+		this.color = color;
+	}
+
+	@Override
+	public String toString() {
+		return "En esta categoria (" + categoria + ") estan autorizados " + autorizados + "y se señaliza con el color" + color;
+	}
+	
+	
 
 }
