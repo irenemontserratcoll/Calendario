3b7bf015bec0cccd7cb0f0ec4a59cad102ec20fc
Idoia Bergera <idoia.bergera@opendeusto.es>
2022-11-02 17:17:45 +0100

    Comienzo EventoTest

--- Calendario/pruebasUnidad/EventoTest.java
+++ Calendario/pruebasUnidad/EventoTest.java
@@ -1,12 +1,30 @@
 import static org.junit.Assert.*;
 
+import java.util.Date;
+
+import org.junit.Before;
 import org.junit.Test;
 
 public class EventoTest {
-
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	/* Esto no me va
+	@Before 
+	public void setUp() {
+		Evento e = new Evento("Estudiar", new Date(7,7,2004,15,30), new Date(8,7,2004,15,45),7.5f, null, true);
 	}
+	 */
+	
+
+	Evento e = new Evento("Estudiar", new Date(7,7,2004,15,30), new Date(8,7,2004,15,45),7.5f, null, true);
+	
+	@Test
+	public void testGetNombre() {
+		assertEquals("Estudiar", e.getNombre());;
+	}
+	@Test
+	public void testSetNombre() {
+		e.setNombre("Trabajo Estadística");
+		assertEquals("Trabajo Estadística", e.getNombre());;
+	}
+
 
 }
