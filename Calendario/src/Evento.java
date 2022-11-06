6d89a117f5d307bbeacd6318a3b41fce347f68f7
Idoia Bergera <idoia.bergera@opendeusto.es>
2022-11-06 14:02:43 +0100

    Realizadas pruebas unidad clase evento 100%

--- Calendario/src/Evento.java
+++ Calendario/src/Evento.java
@@ -2,19 +2,20 @@ import java.time.*;
 import java.time.format.*;
 
 public class Evento {
-	//ATRIBUTOS
+	// ATRIBUTOS
 	protected String nombre;
-	
-	//TODO Ver clase Calendar, Gregorian Calendar y Time (SQL)
-	protected ZonedDateTime fechaInicio; 
+
+	// TODO Ver clase Calendar, Gregorian Calendar y Time (SQL)
+	protected ZonedDateTime fechaInicio;
 	protected ZonedDateTime fechaFin;
 	protected float duracionReal;
 	protected Categoria categoria;
 	protected boolean urgente;
-	//HashCode?
-	
-	
-	/** Construye un evento a partir de todos los datos
+	// HashCode?
+
+	/**
+	 * Construye un evento a partir de todos los datos
+	 * 
 	 * @param nombre
 	 * @param fechaInicio
 	 * @param fechaFin
@@ -22,7 +23,8 @@ public class Evento {
 	 * @param categoria
 	 * @param urgente
 	 */
-	public Evento(String nombre, ZonedDateTime fechaInicio, ZonedDateTime fechaFin, float duracionReal, Categoria categoria, boolean urgente) {
+	public Evento(String nombre, ZonedDateTime fechaInicio, ZonedDateTime fechaFin, float duracionReal,
+			Categoria categoria, boolean urgente) {
 		super();
 		this.nombre = nombre;
 		this.fechaInicio = fechaInicio;
@@ -31,35 +33,59 @@ public class Evento {
 		this.categoria = categoria;
 		this.urgente = urgente;
 	}
-	
-	/** Construye un evento a partir del nombre
+
+	/**
+	 * Construye un evento a partir del nombre
+	 * 
 	 * @param nombre
 	 */
 	public Evento(String nombre) {
 		super();
 		this.fechaInicio = null;
 		this.fechaFin = null;
-		this.duracionReal = 0.F;
+		this.duracionReal = 0.0f;
 		this.categoria = null;
 		this.urgente = false;
 	}
-	
+
+	/**
+	 * Construye un evento con nombre y fecha de inicio a partir de:
+	 * 
+	 * @param nombre Nombre del evento
+	 * @param anyo   Anyo de comienzo del evento
+	 * @param mes    Mes de comienzo del evento
+	 * @param dia    Dia de comienzo del evento
+	 * @param hora   Hora de comienzo del evento
+	 * @param minuto Minuto de comienzo del evento
+	 */
 	public Evento(String nombre, int anyo, int mes, int dia, int hora, int minuto) {
 		super();
 		LocalDateTime dt = LocalDateTime.of(anyo, mes, dia, hora, minuto);
-		ZonedDateTime fechaInicio = ZonedDateTime.of(dt,ZoneId.of("Europe/Madrid"));
-		this.fechaInicio= fechaInicio;
+		ZonedDateTime fechaInicio = ZonedDateTime.of(dt, ZoneId.of("Europe/Madrid"));
+		this.fechaInicio = fechaInicio;
 	}
-	
-	public Evento(String nombre, int anyo, int mes, int dia, int hora, int minuto,int duracionMinutos) {
+
+	/**Construye un evento con nombre, fecha de inicio y fecha de finalización,
+	 * sabiendo la duracion estimada en minutos
+	 * 
+	 * @param nombre Nombre del evento
+	 * @param anyo   Anyo de comienzo del evento
+	 * @param mes    Mes de comienzo del evento
+	 * @param dia    Dia de comienzo del evento
+	 * @param hora   Hora de comienzo del evento
+	 * @param minuto Minuto de comienzo del evento
+	 * @param duracionMinutos Duración del evento en minutos
+	 */
+	public Evento(String nombre, int anyo, int mes, int dia, int hora, int minuto, int duracionMinutos) {
 		super();
+		LocalDateTime dt = LocalDateTime.of(anyo, mes, dia, hora, minuto);
+		ZonedDateTime fechaInicio = ZonedDateTime.of(dt, ZoneId.of("Europe/Madrid"));
 		this.fechaFin = fechaInicio.plusMinutes(duracionMinutos);
 	}
 
-	//METODOS
-	
-	
-	//GETTERS Y SETTERS
+// METODOS
+
+	// GETTERS Y SETTERS
 	public String getNombre() {
 		return nombre;
 	}
@@ -75,11 +101,11 @@ public class Evento {
 	public void setFechaInicio(ZonedDateTime fechaInicio) {
 		this.fechaInicio = fechaInicio;
 	}
-	
+
 	public void setFechaInicio(int anyo, int mes, int dia, int hora, int minuto) {
 		LocalDateTime dt = LocalDateTime.of(anyo, mes, dia, hora, minuto);
-		ZonedDateTime fechaInicio = ZonedDateTime.of(dt,ZoneId.of("Europe/Madrid"));
-		this.fechaInicio= fechaInicio;
+		ZonedDateTime fechaInicio = ZonedDateTime.of(dt, ZoneId.of("Europe/Madrid"));
+		this.fechaInicio = fechaInicio;
 	}
 
 	public ZonedDateTime getFechaFin() {
@@ -90,10 +116,17 @@ public class Evento {
 		this.fechaFin = fechaFin;
 	}
 	
+
 	public void setFechaFin(int duracionMinutos) {
 		this.fechaFin = fechaInicio.plusMinutes(duracionMinutos);
 	}
 
+	public void setFechaFin(int anyo, int mes, int dia, int hora, int minuto) {
+		LocalDateTime dt = LocalDateTime.of(anyo, mes, dia, hora, minuto);
+		ZonedDateTime fechaFin = ZonedDateTime.of(dt, ZoneId.of("Europe/Madrid"));
+		this.fechaFin = fechaFin;
+	}
+	
 	public float getDuracionReal() {
 		return duracionReal;
 	}
@@ -118,4 +151,6 @@ public class Evento {
 		this.urgente = urgente;
 	}
 	
+//TODO Comprobar que la fecha de inicio es anterior a la fecha de fin
+
 }
