6a9fb3b1f6114b4aecf23608b9db1a859b57077c
Idoia Bergera <idoia.bergera@opendeusto.es>
2022-11-14 21:00:47 +0100

    Añadido logger y algunos cambios

--- Calendario/src/baseDeDatos/Usuarios.java
+++ Calendario/src/baseDeDatos/Usuarios.java
@@ -5,40 +5,51 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import clases.Usuario;
+
+import java.util.ArrayList;
+import java.util.logging.Logger;
+
 
 public class Usuarios {
-
-	public static void main(String[] arg) {
-
+	private static Logger logger = Logger.getLogger(Logger.class.getName());
+	private Connection conn;
+	private Statement statement;
+	private ArrayList<Usuario> listaUsuarios;
+	
+	public boolean iniciar(){
 		// Primero cargamos los drivers
 		try {
 			Class.forName("org.sqlite.JDBC");
 		} catch (ClassNotFoundException e) {
-			System.out.println("No se ha podido cargar el driver de la base de datos");
+			logger.severe("No se ha podido cargar el driver de la base de datos: "+ e.getMessage());
 		}
 		// Segundo paso es crear una conexión
-
 		try {
-			Connection conn = DriverManager.getConnection("jdbc:sqlite:Usuarios.db");
+			conn = DriverManager.getConnection("jdbc:sqlite:Usuarios.db");
 			Statement stmt = conn.createStatement();
 
 			try (ResultSet rs = stmt.executeQuery("SELECT Usuario, Contraseña FROM usuarios")) {
 				while (rs.next()) {
 
-					String usuario = rs.getString("Usuario");
+					String nombre = rs.getString("Usuarios");
 					String contraseña = rs.getString("Contraseña");
+					listaUsuarios.add(new Usuario(nombre, contraseña));
 
-					System.out.println(usuario + " " + contraseña);
 				}
 			} catch (SQLException e) {
-				System.out.println("No se ha podido ejecutar la sentencia SQL." + e.getMessage());
+				logger.severe("No se ha podido ejecutar la sentencia SQL" + e.getMessage());
 			}
-
+			logger.info("Conexion con base de datos Usuarios.db establecida");
 			conn.close();
+			return true;
 
 		} catch (SQLException e) {
 			e.printStackTrace();
+			return false;
 		}
 	}
-
+	public static void main(String[] args) {
+		//iniciar();
+	}
 }
