4a5e4a865a3844b6518452edd4b6a52f9f730ef3
Idoia Bergera <idoia.bergera@opendeusto.es>
2022-11-22 17:48:38 +0100

    Añadido checkbox categorias

--- Calendario/src/ventanas/RendererCategoria.java
+++ Calendario/src/ventanas/RendererCategoria.java
@@ -2,17 +2,13 @@ package ventanas;
 
 
 import java.awt.Component;
-import java.awt.FlowLayout;
-import java.awt.event.ItemEvent;
-import java.awt.event.ItemListener;
 import javax.swing.JCheckBox;
 import javax.swing.JList;
-import javax.swing.JPanel;
 import javax.swing.ListCellRenderer;
 
 import clases.Categoria;
 
-public class RendererCategoria extends JPanel implements ListCellRenderer<Categoria> {
+public class RendererCategoria extends JCheckBox implements ListCellRenderer<Categoria> {
 
 	private static final long serialVersionUID = 1L;
 
@@ -20,20 +16,11 @@ public class RendererCategoria extends JPanel implements ListCellRenderer<Catego
 	public Component getListCellRendererComponent(JList<? extends Categoria> list, Categoria value, int index,
 			boolean isSelected, boolean cellHasFocus) {
 		
-		setBackground(value.getColor());
-		setLayout( new FlowLayout());
-		JCheckBox seleccionarCategoria = new JCheckBox(value.getCategoria());
-		seleccionarCategoria.addItemListener(new ItemListener() {
-			
-			@Override
-			public void itemStateChanged(ItemEvent e) {
-				System.out.println(value.getCategoria() + " "+ e.getStateChange());
-				
-			}
-		});
-		seleccionarCategoria.setEnabled(true);
-		add(seleccionarCategoria);
-		
+		setText(value.getCategoria());
+		setForeground(value.getColor());
+		setSelected(true);
+		//TODO no se puede cambiar la seleccion en la ventana
+		setEnabled(true);
 		return this;
 	}
 
