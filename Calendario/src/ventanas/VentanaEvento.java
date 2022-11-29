780ff7add602677d2c2786725150d61d80cda4c2
Nahia Castañeda <nahia.castaneda@opendeusto.es>
2022-11-29 18:52:42 +0100

    Calendario para poner fecha

--- Calendario/src/ventanas/VentanaEvento.java
+++ Calendario/src/ventanas/VentanaEvento.java
@@ -2,18 +2,23 @@ package ventanas;
 
 import java.awt.Color;
 import java.awt.GridLayout;
-import java.time.Month;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.swing.DefaultComboBoxModel;
 import javax.swing.JComboBox;
+import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
+
+import org.jdatepicker.JDateComponentFactory;
+import org.jdatepicker.JDatePicker;
+import org.jdatepicker.impl.JDatePanelImpl;
+import org.jdatepicker.impl.JDatePickerImpl;
+import org.jdatepicker.impl.UtilDateModel;
+
 
 public class VentanaEvento extends JFrame {
 
@@ -22,7 +27,10 @@ public class VentanaEvento extends JFrame {
 	JTextField nombreEvento;
 	JLabel horaIni;
 	JLabel horaFin;
+	JDatePicker depart_date_picker;
+
 	public VentanaEvento() {
+		
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setTitle("Ventana Evento");
 		setSize(400,500);
@@ -35,36 +43,29 @@ public class VentanaEvento extends JFrame {
 		nombreEvento = new JTextField(20);
 		titulo.add(nombreEvento);
 		
-		//Combo box con horas desde las 00 hasta las 23:30 (cada media hora)
-		JPanel horas = new JPanel();
-		horas.setBackground(Color.WHITE);
-		List<String> horasl = new ArrayList<String>();
+		//Combo box con calendario para elegir día
+		JPanel fechaini = new JPanel();
+		JPanel fechafin = new JPanel();
+		 JDatePicker picker = new JDateComponentFactory().createJDatePicker();
+		    picker.setTextEditable(true);
+		    picker.setShowYearButtons(true);
+
+		 JDatePicker picker1 = new JDateComponentFactory().createJDatePicker();
+		 	picker1.setTextEditable(true);
+		    picker1.setShowYearButtons(true);
+
+		horaIni = new JLabel("Fecha inicio");
+		horaFin = new JLabel("Fecha fin");
+		fechaini.add(horaIni);
+		fechaini.add((JComponent) picker);
+		fechafin.add(horaFin);
+	    fechafin.add((JComponent) picker1);
+
 	
-		for (int i=0;i<24;i++) {
-			horasl.add(i + ":00");
-			horasl.add(i + ":30");
-		}
-		JComboBox<Object> horasInicio = new JComboBox<Object>();
-	
-		for(int i=0;i<horasl.size();i++){
-			horasInicio.addItem(horasl.get(i));
-		}
-		JComboBox<Object> horasFin = new JComboBox<Object>();
-	
-		for(int i=0;i<horasl.size();i++){
-			horasFin.addItem(horasl.get(i));
-		}	
-		horaIni = new JLabel("Hora inicio");
-		horaFin = new JLabel("Hora fin");
-		horas.add(horaIni);
-		horas.add(horasInicio);
-		horas.add(horaFin);
-		horas.add(horasFin);
-	
-	
-	
+		
 	principal.add(titulo);
-	principal.add(horas);
+	principal.add(fechaini);
+	principal.add(fechafin);
 	add(principal);
 	setVisible(true);
 }
