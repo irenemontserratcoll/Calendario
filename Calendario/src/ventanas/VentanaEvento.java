a1bd9607dd83c183bee3f1865d746747e392b40b
Nahia Castañeda <nahia.castaneda@opendeusto.es>
2022-12-04 11:56:17 +0100

    Ventana evento categoria y seleccionar si es urgente o no

--- Calendario/src/ventanas/VentanaEvento.java
+++ Calendario/src/ventanas/VentanaEvento.java
@@ -1,22 +1,24 @@
 package ventanas;
 
-
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.swing.ButtonGroup;
+import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JRadioButton;
 import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
 
 import org.jdatepicker.JDateComponentFactory;
 import org.jdatepicker.JDatePicker;
-
-
 
 public class VentanaEvento extends JFrame {
 
@@ -26,51 +28,52 @@ public class VentanaEvento extends JFrame {
 	JLabel horaIni;
 	JLabel horaFin;
 	JDatePicker depart_date_picker;
+	JButton bEv;
+	private JRadioButton si,no;
 
 	public VentanaEvento() {
-		
+
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setTitle("Ventana Evento");
-		setSize(400,500);
-		JPanel principal = new JPanel(new GridLayout(6,1));
-		
-		//Titulo evento
+		setSize(400, 500);
+		JPanel principal = new JPanel(new GridLayout(6, 1));
+
+		// Titulo evento
 		JPanel titulo = new JPanel();
 		textoEvento = new JLabel("Evento");
 		titulo.add(textoEvento);
 		nombreEvento = new JTextField(20);
 		titulo.add(nombreEvento);
-		
-		//Combo box con calendario para elegir día
+
+		// Combo box con calendario para elegir día
 		JPanel fechaini = new JPanel();
 		JPanel fechafin = new JPanel();
-		
-		//Combo box con horas desde las 00 hasta las 23:30 (cada media hora)
-				JPanel horas = new JPanel();
-				List<String> horasl = new ArrayList<String>();
-			
-				for (int i=0;i<24;i++) {
-					horasl.add(i + ":00");
-					horasl.add(i + ":30");
-				}
-				JComboBox<Object> horasInicio = new JComboBox<Object>();
-			
-				for(int i=0;i<horasl.size();i++){
-					horasInicio.addItem(horasl.get(i));
-				}
-				JComboBox<Object> horasFin = new JComboBox<Object>();
-			
-				for(int i=0;i<horasl.size();i++){
-					horasFin.addItem(horasl.get(i));
-				}
-		
-		 JDatePicker picker = new JDateComponentFactory().createJDatePicker();
-		    picker.setTextEditable(true);
-		    picker.setShowYearButtons(true);
 
-		 JDatePicker picker1 = new JDateComponentFactory().createJDatePicker();
-		 	picker1.setTextEditable(true);
-		    picker1.setShowYearButtons(true);
+		// Combo box con horas desde las 00 hasta las 23:30 (cada media hora)
+		List<String> horasl = new ArrayList<String>();
+
+		for (int i = 0; i < 24; i++) {
+			horasl.add(i + ":00");
+			horasl.add(i + ":30");
+		}
+		JComboBox<Object> horasInicio = new JComboBox<Object>();
+
+		for (int i = 0; i < horasl.size(); i++) {
+			horasInicio.addItem(horasl.get(i));
+		}
+		JComboBox<Object> horasFin = new JComboBox<Object>();
+
+		for (int i = 0; i < horasl.size(); i++) {
+			horasFin.addItem(horasl.get(i));
+		}
+
+		JDatePicker picker = new JDateComponentFactory().createJDatePicker();
+		picker.setTextEditable(true);
+		picker.setShowYearButtons(true);
+
+		JDatePicker picker1 = new JDateComponentFactory().createJDatePicker();
+		picker1.setTextEditable(true);
+		picker1.setShowYearButtons(true);
 
 		horaIni = new JLabel("Fecha inicio");
 		horaFin = new JLabel("Fecha fin");
@@ -78,23 +81,58 @@ public class VentanaEvento extends JFrame {
 		fechaini.add((JComponent) picker);
 		fechaini.add(horasInicio);
 		fechafin.add(horaFin);
-	    fechafin.add((JComponent) picker1);
-	    fechafin.add(horasFin);
-
-	
+		fechafin.add((JComponent) picker1);
+		fechafin.add(horasFin);
 		
-	principal.add(titulo);
-	principal.add(fechaini);
-	principal.add(fechafin);
-	add(principal);
-	setVisible(true);
-}
+		// Categoria
+		JPanel categoria = new JPanel();
+		JLabel categ = new JLabel("Categoria");
+		JTextField categorias = new JTextField(20);
+		
+		
+		categoria.add(categ);
+		categoria.add(categorias);
+		//Urgente si/no
+		ButtonGroup grupo = new ButtonGroup();
+		JPanel urgente = new JPanel();
+		JLabel urgente1 = new JLabel("¿Es urgente?");
+		si = new JRadioButton("SI");
+		no = new JRadioButton("NO");
+		urgente.add(urgente1);
+		grupo.add(si);
+		grupo.add(no);
+		urgente.add(si);
+		urgente.add(no);
+		// Botón crear evento
+		JPanel boton = new JPanel();
+		bEv = new JButton("Crear evento");
 
-public static void main(String[] args) {
-	SwingUtilities.invokeLater(new Runnable() {
-		public void run() {
-			new VentanaEvento();
-		}
-	});
-}
+		bEv.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
+
+		boton.add(bEv);
+
+		principal.add(titulo);
+		principal.add(fechaini);
+		principal.add(fechafin);
+		principal.add(categoria);
+		principal.add(urgente);
+		principal.add(boton);
+		
+		add(principal);
+		setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				new VentanaEvento();
+			}
+		});
+	}
 }
