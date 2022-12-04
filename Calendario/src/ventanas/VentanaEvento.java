17a47e3c1baec2cc8e8fd651699f4a017b9451cb
Nahia Castañeda <nahia.castaneda@opendeusto.es>
2022-12-04 13:04:25 +0100

    Quitar warnings ventana evento

--- Calendario/src/ventanas/VentanaEvento.java
+++ Calendario/src/ventanas/VentanaEvento.java
@@ -10,7 +10,6 @@ import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JComponent;
-import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
