cb2e3e244f9e504b8b740d25977df2665c562661
Nahia Castañeda <nahia.castaneda@opendeusto.es>
2023-01-17 22:28:19 +0100

    Test Usuario al 100%

--- Calendario/tests/clases/UsuarioTest.java
+++ Calendario/tests/clases/UsuarioTest.java
@@ -3,10 +3,21 @@ package clases;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
+import org.junit.Before;
+
 public class UsuarioTest {
-
+	public Usuario user1;
+	public Usuario user2;
+	public Usuario user3;
+	public Usuario user4;
    
-
+	@Before
+	public void setUp() {
+		user1 = new Usuario("Nahia", "password");
+        user2 = new Usuario("Nahia", "password");
+        user3 = new Usuario("Prueba", "password");
+        user4 = new Usuario("Nahia", "pass");
+	}
     @Test
     public void testSetNombre() {
         Usuario usuario = new Usuario("Juan", "1234");
@@ -20,5 +31,21 @@ public class UsuarioTest {
         usuario.setContraseña("4321");
         assertEquals("4321", usuario.getContraseña());
     }
+    @Test
+    public void testHashCode() {
+        assertEquals(user1.hashCode(), user2.hashCode());
+        assertNotEquals(user1.hashCode(), user3.hashCode());
+        assertNotEquals(user1.hashCode(), user4.hashCode());
+    }
 
+    @Test
+    public void testEquals() {
+    	
+    	assertTrue(user1.equals(user2));
+    	assertTrue(user1.equals(user1));
+        assertFalse(user1.equals(user3));
+        assertFalse(user1.equals(user4));
+        assertFalse(user1.equals(null));
+        assertFalse(user1.equals("John"));
+    }
 }
